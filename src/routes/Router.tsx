3b0ee448be882f04d7b14f8978792182dc0baee1
Lilly <myk19977@gmail.com>
2024-03-27 11:43:17 +0900

    ♻️ refactoring

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Top from '@/features/top/routes';
 import Under from '@/features/under/routes';
+import NotFound from '@/features/notFound/routes';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Top />} />
       <Route path="/under" element={<Under />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
